a9d9927446f95a3e28eb93495ae88623e5292ff0
Slavashevich <aslavashevich@ibagroup.eu>
2023-07-25 16:43:35 +0300

    init monorepo with react express

---
+++ client/src/components/App.tsx
@@ -0,0 +1,9 @@
+import { test } from 'shared';
+
+const App = () => {
+  console.log(test);
+
+  return <div className="flex justify-center bg-gray-300">hello world</div>;
+};
+
+export default App;
